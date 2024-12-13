a39545b79a6a546ffd1ea8e58f8a5c76a945e5e2
QwQ-dev <qwqdev@gmail.com>
2024-12-13 13:12:23 +0800

    chore: Add GsonUtils.java.

---
+++ src/main/java/me/qwqdev/template/utils/GsonUtils.java
@@ -0,0 +1,16 @@
+package me.qwqdev.template.utils;
+
+import com.google.gson.Gson;
+
+/**
+ * The type Gson utils.
+ *
+ * @author qwq-dev
+ * @since 2024-12-13 11:53
+ */
+public class GsonUtils {
+    /**
+     * The constant GSON.
+     */
+    public static Gson GSON = new Gson();
+}
